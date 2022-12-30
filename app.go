fb18199b0a121508d7bb46ff67e920544ee70350
Simonexc <szymekdt@gmail.com>
2022-12-30 19:55:28 +0100

    Update hook

--- app.go
+++ app.go
@@ -31,7 +31,7 @@ func NewApp(repo string, branch string, mainFolder string) *App {
 
 func (a *App) initRepo() error {
 	_, err := os.Stat(a.RepoFolder)
-	if os.IsExist(err) {
+	if !os.IsNotExist(err) {
 		log.Print("Repository already initialized")
 		return nil
 	}
